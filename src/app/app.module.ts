bd433bcd8978f58c2501bac7168fee85a62699f2
Aleksey Nikitin <aleksey@noblecode.ru>
2019-03-14 11:24:11 +0400

    Подключил vibor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgViborModule } from 'ng-vibor';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, NgViborModule
   ],
   providers: [],
   bootstrap: [AppComponent]
