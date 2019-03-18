44eb475e4cc7e0d7d6bef83ca7dc3a1630afbae8
Aleksey Nikitin <aleksey@noblecode.ru>
2019-03-18 12:07:38 +0400

    Немного форматирования

--- projects/ng-vibor/src/lib/components/options-viewer/options-viewer.component.ts
+++ projects/ng-vibor/src/lib/components/options-viewer/options-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, } from '@angular/core';
+import { Component } from '@angular/core';
 import { DataSource, CollectionViewer } from '@angular/cdk/collections';
 
 import { BehaviorSubject, Subscription, Observable } from 'rxjs';
@@ -9,51 +9,52 @@ import { BehaviorSubject, Subscription, Observable } from 'rxjs';
     styleUrls: ['./options-viewer.component.scss']
 })
 export class OptionsViewerComponent {
-    ds = new MyDataSource();
+    public ds = new MyDataSource();
+    public selectedItem: any;
 
-    constructor() {}
+    constructor() { }
 }
 
 
 export class MyDataSource extends DataSource<string | undefined> {
     private length = 100000;
     private pageSize = 100;
-    private cachedData = Array.from<string>({length: this.length});
+    private cachedData = Array.from<string>({ length: this.length });
     private fetchedPages = new Set<number>();
     private dataStream = new BehaviorSubject<(string | undefined)[]>(this.cachedData);
     private subscription = new Subscription();
 
     connect(collectionViewer: CollectionViewer): Observable<(string | undefined)[]> {
-      this.subscription.add(collectionViewer.viewChange.subscribe(range => {
-        const startPage = this.getPageForIndex(range.start);
-        const endPage = this.getPageForIndex(range.end - 1);
-        for (let i = startPage; i <= endPage; i++) {
-          this.fetchPage(i);
-        }
-      }));
-      return this.dataStream;
+        this.subscription.add(collectionViewer.viewChange.subscribe(range => {
+            const startPage = this.getPageForIndex(range.start);
+            const endPage = this.getPageForIndex(range.end - 1);
+            for (let i = startPage; i <= endPage; i++) {
+                this.fetchPage(i);
+            }
+        }));
+        return this.dataStream;
     }
 
     disconnect(): void {
-      this.subscription.unsubscribe();
+        this.subscription.unsubscribe();
     }
 
     private getPageForIndex(index: number): number {
-      return Math.floor(index / this.pageSize);
+        return Math.floor(index / this.pageSize);
     }
 
     private fetchPage(page: number) {
-      if (this.fetchedPages.has(page)) {
-        return;
-      }
-      this.fetchedPages.add(page);
+        if (this.fetchedPages.has(page)) {
+            return;
+        }
+        this.fetchedPages.add(page);
 
-      // Use `setTimeout` to simulate fetching data from server.
-      setTimeout(() => {
-        this.cachedData.splice(page * this.pageSize, this.pageSize,
-            ...Array.from({length: this.pageSize})
-                .map((_, i) => `Options #${page * this.pageSize + i}`));
-        this.dataStream.next(this.cachedData);
-      }, Math.random() * 1000 + 200);
+        // Use `setTimeout` to simulate fetching data from server.
+        setTimeout(() => {
+            this.cachedData.splice(page * this.pageSize, this.pageSize,
+                ...Array.from({ length: this.pageSize })
+                    .map((_, i) => `Options #${page * this.pageSize + i}`));
+            this.dataStream.next(this.cachedData);
+        }, Math.random() * 1000 + 200);
     }
-  }
+}
