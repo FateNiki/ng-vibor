3a63c5fa677f21511ec7f01ae8123bbd4c5d7e83
Aleksey Nikitin <aleksey@noblecode.ru>
2019-03-19 13:48:30 +0400

    Изменил демо пример

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { ArrayConnector } from 'ng-vibor';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'vibor-demo';
+    title = 'vibor-demo';
+    public arrayConnector = new ArrayConnector<number, number>(Array.from({length: 500}).map(Math.random), 50);
 }
