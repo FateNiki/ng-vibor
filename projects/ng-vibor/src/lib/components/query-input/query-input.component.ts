ef8cdc1535d55cdce1b9db3f0cf4a3216bf01d09
Aleksey Nikitin <aleksey@noblecode.ru>
2019-03-19 11:14:01 +0400

    Отпрвка сигналов в сервис

--- projects/ng-vibor/src/lib/components/query-input/query-input.component.ts
+++ projects/ng-vibor/src/lib/components/query-input/query-input.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { NgViborService } from '../../services/ng-vibor.service';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
     selector: 'vibor-query-input',
     templateUrl: './query-input.component.html',
-    styleUrls: ['./query-input.component.scss']
+    styleUrls: ['./query-input.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class QueryInputComponent {
     static readonly emittedKey = ['ArrowUp', 'ArrowDown', 'Enter'];
 
-    constructor(private vs: NgViborService) { }
+    public query = new FormControl(undefined);
+
+    constructor(private vs: NgViborService) {
+        this.query.valueChanges.pipe(
+            debounceTime(300),
+            distinctUntilChanged()
+        ).subscribe(
+            newValue => this.vs.query.next(newValue)
+        );
+    }
 
     public EmitKeyPress(event: KeyboardEvent): void {
         if (QueryInputComponent.emittedKey.includes(event.key)) {
--- projects/ng-vibor/src/lib/ng-vibor.module.ts
+++ projects/ng-vibor/src/lib/ng-vibor.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
-import {ScrollingModule} from '@angular/cdk/scrolling';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 import { NgViborComponent } from './components/ng-vibor.component';
 import { OptionsViewerComponent } from './components/options-viewer/options-viewer.component';
 import { QueryInputComponent } from './components/query-input/query-input.component';
 
 @NgModule({
-  declarations: [NgViborComponent, OptionsViewerComponent, QueryInputComponent],
-  imports: [
-    ScrollingModule
-  ],
-  exports: [NgViborComponent]
+    declarations: [
+        NgViborComponent,
+        OptionsViewerComponent,
+        QueryInputComponent
+    ],
+    imports: [
+        ReactiveFormsModule,
+        ScrollingModule
+    ],
+    exports: [
+        NgViborComponent
+    ]
 })
 export class NgViborModule { }
