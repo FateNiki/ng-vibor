cc0f61bc2532ce417ec3bb47fa2439c1432c0ba5
Aleksey Nikitin <aleksey@noblecode.ru>
2019-03-21 17:23:58 +0400

    Начал внедрять ControlValueAccessor

--- projects/ng-vibor/src/lib/components/vibor-select/vibor-select.component.ts
+++ projects/ng-vibor/src/lib/components/vibor-select/vibor-select.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+import { Component, Input, OnInit, forwardRef } from '@angular/core';
+
+export const VIBOR_VALUE_ACCESSOR: any = {
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => ViborSelectComponent),
+    multi: true
+};
 
 @Component({
-  selector: 'vibor-vibor-select',
-  templateUrl: './vibor-select.component.html',
-  styleUrls: ['./vibor-select.component.scss']
+    selector: 'vibor-select',
+    templateUrl: './vibor-select.component.html',
+    styleUrls: ['./vibor-select.component.scss'],
+    providers: [VIBOR_VALUE_ACCESSOR]
 })
-export class ViborSelectComponent implements OnInit {
+export class ViborSelectComponent<SModel, FModel> implements OnInit, ControlValueAccessor {
+    private localValue: FModel;
 
-  constructor() { }
+    set value(value: any) {
+        this.localValue = value;
+        this.notifyValueChange();
+    }
 
-  ngOnInit() {
-  }
+    get value(): any {
+        return this.localValue;
+    }
 
+    constructor() { }
+
+    notifyValueChange(): void {
+        if (this.onChange) {
+            this.onChange(this.value);
+        }
+    }
+
+    ngOnInit(): void {
+
+    }
+
+    writeValue(obj: any): void {
+        this.localValue = obj;
+    }
+
+    setDisabledState(isDisabled: boolean): void {
+    }
+
+    // Declaration events
+    onChange: (value) => {};
+    onTouched: () => {};
+
+    // Register events
+    registerOnChange(fn: any): void { this.onChange = fn; }
+    registerOnTouched(fn: any): void { this.onTouched = fn; }
 }
