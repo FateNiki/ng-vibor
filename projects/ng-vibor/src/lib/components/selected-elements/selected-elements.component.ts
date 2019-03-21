d7f7025a4e058b9956d4c696f8727d9754642799
Aleksey Nikitin <aleksey@noblecode.ru>
2019-03-21 16:32:23 +0400

    Компоненты для отображения элементов

---
+++ projects/ng-vibor/src/lib/components/selected-elements/selected-elements.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vibor-selected-elements',
+  templateUrl: './selected-elements.component.html',
+  styleUrls: ['./selected-elements.component.scss']
+})
+export class SelectedElementsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- projects/ng-vibor/src/lib/ng-vibor.module.ts
+++ projects/ng-vibor/src/lib/ng-vibor.module.ts
@@ -7,13 +7,15 @@ import { TestViborComponent } from './components/ng-vibor-test.component';
 import { OptionsViewerComponent } from './components/options-viewer/options-viewer.component';
 import { QueryInputComponent } from './components/query-input/query-input.component';
 import { ViborSelectComponent } from './components/vibor-select/vibor-select.component';
+import { SelectedElementsComponent } from './components/selected-elements/selected-elements.component';
 
 @NgModule({
     declarations: [
         TestViborComponent,
         OptionsViewerComponent,
         QueryInputComponent,
-        ViborSelectComponent
+        ViborSelectComponent,
+        SelectedElementsComponent
     ],
     imports: [
         CommonModule,
