1e3c2195715d3a742efced1f10bdad86af46f5b3
Aleksey Nikitin <aleksey@noblecode.ru>
2019-03-22 15:14:12 +0400

    Начал чинить тестирование в библиотеке

--- projects/ng-vibor/src/lib/helpers/connector.spec.ts
+++ projects/ng-vibor/src/lib/helpers/connector.spec.ts
@@ -2,22 +2,21 @@ import { ArrayConnector } from './array.connector';
 
 describe('Connector', () => {
     const limit = 12;
-    let connector: ArrayConnector<number>;
+    let connector: ArrayConnector<number, number>;
 
     beforeEach(() => {
         const array = (new Array(111)).fill(Math.random());
-        connector = new ArrayConnector<number>(array, limit);
+        connector = new ArrayConnector<number, number>(array, limit);
     });
 
     it('Created', () => {
-      expect(connector).toBeTruthy();
+        expect(connector).toBeTruthy();
     });
 
     it('First page is Load', (done) => {
-        connector.ForceUpdate();
-        connector.value$.subscribe(newValue => {
+        connector.GetList(undefined, 0).subscribe(newValue => {
             expect(newValue.data.length).toBe(limit);
             done();
         });
     });
-  });
+});
